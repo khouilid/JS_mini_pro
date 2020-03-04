ef4a7385edaff97f428f094cd2ddd723b64bc4ea
abdelali-code <57986145+abdelali-code@users.noreply.github.com>
2020-03-04 11:24:21 +0100

    add an initial design of the footer

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let Menu = document.getElementById("menu");
-let Menu2 = document.getElementById("menu2");
+const email = document.getElementById('email');
+const btn = document.getElementById('btn');
+const spanErr = document.querySelector('.error');
+let error;
+const regex = /[a-zA-Z0-9._\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,4}/;
 
+btn.addEventListener('click', () => {
+    if (!regex.test(email.value)) {
+        spanErr.textContent = "the email enterd is not valid ";
+    }
+    else {
+        spanErr.textContent = '';
+    }
+})
 
-Menu.addEventListener("click", function () {
-    document.getElementById("navList2").style.display = "block";
-    document.getElementById("menu2").style.display = "block";
-    document.getElementById("menu").style.display = "none";
-
-}) 
-Menu2.addEventListener("click", function () {
-    document.getElementById("navList2").style.display = "none";
-    document.getElementById("menu").style.display = "block";
-    document.getElementById("menu2").style.display = "none";
-
-}) 
